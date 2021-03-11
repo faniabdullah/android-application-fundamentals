beb9715aa1b0059ca8b2f1fea585b65492c1b699
faniabdullah <faniabdullah27@gmail.com>
2021-03-11 23:35:31 +0700

    delete some useful code

--- app/src/main/java/com/bangkit/faniabdullah_bfaa/MainActivity.kt
+++ app/src/main/java/com/bangkit/faniabdullah_bfaa/MainActivity.kt
@@ -8,32 +8,13 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var myButton: MyButton
-    private lateinit var myEditText: MyEditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myButton = findViewById(R.id.my_button)
-        myEditText = findViewById(R.id.my_edit_text)
-        setMyButtonEnable()
-
-        myEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-            }
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                setMyButtonEnable()
-            }
-            override fun afterTextChanged(s: Editable) {
-            }
-        })
-        myButton.setOnClickListener { Toast.makeText(this@MainActivity, myEditText.text, Toast.LENGTH_SHORT).show() }
     }
 
-    private fun setMyButtonEnable() {
-        val result = myEditText.text
-        myButton.isEnabled = result != null && result.toString().isNotEmpty()
-    }
 
 }
