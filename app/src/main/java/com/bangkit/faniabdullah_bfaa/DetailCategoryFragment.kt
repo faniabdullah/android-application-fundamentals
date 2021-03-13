f17ec3c33c506bf88799a6d871b260e8dba18369
faniabdullah <faniabdullah27@gmail.com>
2021-03-13 21:24:30 +0700

    navigation practice

--- app/src/main/java/com/bangkit/faniabdullah_bfaa/DetailCategoryFragment.kt
+++ app/src/main/java/com/bangkit/faniabdullah_bfaa/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.bangkit.faniabdullah_bfaa.databinding.FragmentDetailCategoryBinding
 
 class DetailCategoryFragment : Fragment() {
@@ -27,6 +28,10 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Stock : $dataDescription"
+
+        binding.btnProfile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 
     override fun onDestroy() {
