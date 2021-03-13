33dfe6b9e5550e69b112a64bcee1fc171beaad4a
faniabdullah <faniabdullah27@gmail.com>
2021-03-13 20:36:10 +0700

    search view practice

--- app/src/main/java/com/bangkit/faniabdullah_bfaa/MainActivity.kt
+++ app/src/main/java/com/bangkit/faniabdullah_bfaa/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.bangkit.faniabdullah_bfaa
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import com.bangkit.faniabdullah_bfaa.databinding.ActivityMainBinding
 
 
@@ -21,6 +25,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu ( menu : Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu,menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
         return true
     }
 
