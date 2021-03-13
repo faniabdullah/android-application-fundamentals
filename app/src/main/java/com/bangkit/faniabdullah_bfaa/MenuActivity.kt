e104b3023050311624136f679ee22b8144279219
faniabdullah <faniabdullah27@gmail.com>
2021-03-13 21:06:57 +0700

    option menu practice

---
+++ app/src/main/java/com/bangkit/faniabdullah_bfaa/MenuActivity.kt
@@ -0,0 +1,11 @@
+package com.bangkit.faniabdullah_bfaa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
