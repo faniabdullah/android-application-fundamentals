e27ddabffb7cd24cec78186704a13638c6b04e41
faniabdullah <faniabdullah27@gmail.com>
2021-03-16 09:34:12 +0700

    initial commit for MVVVM

--- app/src/main/java/com/bangkit/faniabdullah_bfaa/MainViewModel.kt
+++ app/src/main/java/com/bangkit/faniabdullah_bfaa/MainViewModel.kt
@@ -1,50 +1,25 @@
 package com.bangkit.faniabdullah_bfaa
 
-import android.os.Bundle
 import android.util.Log
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.bangkit.faniabdullah_bfaa.databinding.ActivityMainBinding
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import cz.msebera.android.httpclient.Header
 import org.json.JSONObject
 import java.text.DecimalFormat
 
-class MainActivity : AppCompatActivity()  {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: WeatherAdapter
-    companion object {
-        private const val JOB_ID = 10
+class MainViewModel : ViewModel() {
+    val listWeathers = MutableLiveData<ArrayList<WeatherItems>>()
+
+    fun getWeather(cities: String): LiveData<ArrayList<WeatherItems>> {
+        return listWeathers
     }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        adapter = WeatherAdapter()
-        adapter.notifyDataSetChanged()
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter = adapter
-        mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
-
-
-        binding.btnCity.setOnClickListener {
-            val city = binding.editCity.text.toString()
-            if (city.isEmpty()) return@setOnClickListener
-            showLoading(true)
-            setWeather(city)
-        }
-
-    }
-
-    private fun setWeather(city: String) {
+    fun setWeather(cities: String) {
         val listItems = ArrayList<WeatherItems>()
-        val apiKey = "4c47512a1e1c689510ae751023f05916"
-
-        val url = "https://api.openweathermap.org/data/2.5/group?id=${city}&units=metric&appid=${apiKey}"
+        val apiKey = "ISI SESUAI API KEY ANDA"
+        val url = "https://api.openweathermap.org/data/2.5/group?id=${cities}&units=metric&appid=${apiKey}"
         val client = AsyncHttpClient()
         client.get(url, object : AsyncHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Array<Header>, responseBody: ByteArray) {
@@ -65,9 +40,7 @@ class MainActivity : AppCompatActivity()  {
                         weatherItems.temperature = DecimalFormat("##.##").format(tempInCelsius)
                         listItems.add(weatherItems)
                     }
-                    //set data ke adapter
-                    adapter.setData(listItems)
-                    showLoading(false)
+                    listWeathers.postValue(listItems)
                 } catch (e: Exception) {
                     Log.d("Exception", e.message.toString())
                 }
@@ -76,13 +49,5 @@ class MainActivity : AppCompatActivity()  {
                 Log.d("onFailure", error.message.toString())
             }
         })
-    }
-
-    private fun showLoading(state: Boolean) {
-        if (state) {
-            binding.progressBar.visibility = View.VISIBLE
-        } else {
-            binding.progressBar.visibility = View.GONE
-        }
     }
 }
