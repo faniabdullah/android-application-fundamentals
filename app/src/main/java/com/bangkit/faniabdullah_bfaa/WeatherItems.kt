84c8aaebfd221970deb8eca89e0ccf11dcc10166
faniabdullah <faniabdullah27@gmail.com>
2021-03-18 23:21:03 +0700

    initial commit for submission 2

--- app/src/main/java/com/bangkit/faniabdullah_bfaa/WeatherItems.kt
+++
@@ -1,9 +0,0 @@
-package com.bangkit.faniabdullah_bfaa
-
-class WeatherItems {
-    var id: Int = 0
-    var name: String? = null
-    var currentWeather: String? = null
-    var description: String? = null
-    var temperature: String? = null
-}
