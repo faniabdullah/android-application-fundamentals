fadf24e63b41fe592d4caa880d602b8c73c72c32
faniabdullah <faniabdullah27@gmail.com>
2021-03-24 23:42:14 +0700

    clean up code

--- app/src/main/java/com/bangkit/faniabdullah_bfaa/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/bangkit/faniabdullah_bfaa/ui/favorite/FavoriteViewModel.kt
@@ -3,11 +3,9 @@ package com.bangkit.faniabdullah_bfaa.ui.favorite
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.bangkit.faniabdullah_bfaa.data.local.FavoriteUser
 import com.bangkit.faniabdullah_bfaa.data.local.FavoriteUserDao
 import com.bangkit.faniabdullah_bfaa.data.local.UserDatabase
-import com.bangkit.faniabdullah_bfaa.domain.model.User
 
 class FavoriteViewModel (application: Application) : AndroidViewModel(application) {
 
