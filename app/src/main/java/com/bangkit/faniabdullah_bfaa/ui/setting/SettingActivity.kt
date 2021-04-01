d3a825a3dda4611a58ae172ef72999c23e5ab94b
faniabdullah <faniabdullah27@gmail.com>
2021-04-01 15:01:33 +0700

    setting activity view

---
+++ app/src/main/java/com/bangkit/faniabdullah_bfaa/ui/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.faniabdullah_bfaa.ui.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.faniabdullah_bfaa.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
