f3b36505721d777cf56a0d9fc0ceccb1fc1c19db
faniabdullah <faniabdullah27@gmail.com>
2021-04-01 14:17:41 +0700

    clean up code

--- app/src/main/java/com/bangkit/faniabdullah_bfaa/data/network/RetrofitClient.kt
+++ app/src/main/java/com/bangkit/faniabdullah_bfaa/data/network/RetrofitClient.kt
@@ -5,11 +5,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private  const val BASE_URL = "https://api.github.com/"
-    val retrofit = Retrofit.Builder()
+    private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    val apiInstance = retrofit.create(Api::class.java)
+    val apiInstance: Api = retrofit.create(Api::class.java)
 
 }
