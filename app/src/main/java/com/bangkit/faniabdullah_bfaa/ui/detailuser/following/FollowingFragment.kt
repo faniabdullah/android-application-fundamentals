9e4f55bda399667a7eb0ec12991cd6b936229b09
faniabdullah <faniabdullah27@gmail.com>
2021-04-05 14:30:27 +0700

    clean up code

--- app/src/main/java/com/bangkit/faniabdullah_bfaa/ui/detailuser/following/FollowingFragment.kt
+++ app/src/main/java/com/bangkit/faniabdullah_bfaa/ui/detailuser/following/FollowingFragment.kt
@@ -29,8 +29,7 @@ class FollowingFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentFollowingBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
