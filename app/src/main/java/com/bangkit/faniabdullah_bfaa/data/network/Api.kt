1f0dd7f43ced549efa21f5c8f1fc125cd9699bf5
faniabdullah <faniabdullah27@gmail.com>
2021-04-08 23:25:09 +0700

    suggestion reviewer

--- app/src/main/java/com/bangkit/faniabdullah_bfaa/data/network/Api.kt
+++ app/src/main/java/com/bangkit/faniabdullah_bfaa/data/network/Api.kt
@@ -1,5 +1,6 @@
 package com.bangkit.faniabdullah_bfaa.data.network
 
+import com.bangkit.faniabdullah_bfaa.BuildConfig
 import com.bangkit.faniabdullah_bfaa.domain.model.DetailUserResponse
 import com.bangkit.faniabdullah_bfaa.domain.model.RepositoriesResponse
 import com.bangkit.faniabdullah_bfaa.domain.model.User
@@ -12,32 +13,32 @@ import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token 6f450176356630e2240ca9a8929a322f628c1bb1")
+    @Headers("Authorization: ${BuildConfig.GITHUB_TOKEN}")
     fun getSearchUsers(
         @Query("q") query: String,
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 6f450176356630e2240ca9a8929a322f628c1bb1")
+    @Headers("Authorization: ${BuildConfig.GITHUB_TOKEN}")
     fun getDetailUsers(
         @Path("username") username: String,
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 6f450176356630e2240ca9a8929a322f628c1bb1")
+    @Headers("Authorization: ${BuildConfig.GITHUB_TOKEN}")
     fun getFollowingUsers(
         @Path("username") username: String,
     ): Call<ArrayList<User>>
 
 
     @GET("users/{username}/repos")
-    @Headers("Authorization: token 6f450176356630e2240ca9a8929a322f628c1bb1")
+    @Headers("Authorization: ${BuildConfig.GITHUB_TOKEN}")
     fun getRepositoriesUsers(
         @Path("username") username: String,
     ): Call<ArrayList<RepositoriesResponse>>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 6f450176356630e2240ca9a8929a322f628c1bb1")
+    @Headers("Authorization: ${BuildConfig.GITHUB_TOKEN}")
     fun getFollowersUsers(
         @Path("username") username: String,
     ): Call<ArrayList<User>>
